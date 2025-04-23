f9a2862e08daa2f948866df375bb4e671f75b2fd
sumu2025 <weipeacock@gmail.com>
2025-04-24 00:39:46 +0800

    添加 Node.js 服务端

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/' || req.url === '/index.html') {
+        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+            if (err) {
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('服务器内部错误');
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                res.end(data);
+            }
+        });
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('未找到页面');
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`服务器运行在 http://${hostname}:${port}/`);
+});
